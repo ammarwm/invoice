87b91000b54a42c1c79fc8d9cbc98d567036cf20
Ammar Mohemmed <ammarwm@gmail.com>
2015-10-11 20:38:47 +1300

    printing direct from invoice

--- app.js
+++ app.js
@@ -3,70 +3,76 @@
  */
 
 (function(){
-    var app = angular.module("invoice", ['ngRoute']);
-    
-    app.config(['$routeProvider',function ($routeProvider, $locationProvider){
+    var app = angular.module("invoice", ['ngRoute','flow']);
+    app.config(['$routeProvider',function($routeProvider, $locationProvider){
            $routeProvider
            .when("/invoice",{
-                controller:"itemCtrl",
-                templateUrl:"/invoice/invoice.html"
+                controller:"invoiceCtrl",
+                templateUrl:"invoice.html"
            })
            .when("/email",{
                 controller:"itemList",
-                templateUrl:"/invoice/email.html"
+                templateUrl:"email.html"
            })
                .otherwise({
-                controller:"itemCtrl",
-                templateUrl:"/invoice/invoice.html"
-
+                controller:"invoiceCtrl",
+                templateUrl:"invoice.html"
            })
     }
     ]);
     
-    app.controller("itemCtrl", function($scope, $location, emailService){
-        $scope.header= {to_add:"",to:"",number:"", date:"", pdate:""};
+    app.controller("invoiceCtrl", function($scope, $location, emailService){
+        $scope.header= {to_add:"",to:"",number:"", date:"", pdate:"",total:0,paid:0,balance:0,freight:0};
         $scope.items = [{code:"", desc:"", qty:0,price:0,amount:0}];
-        $scope.total = 0;
         this.delItem = function(item){
-                        $scope.total -= $scope.items[item].amount;
+                        $scope.header.total -= $scope.items[item].amount;
                         $scope.items.splice(item,1);
-    };
-    this.addItem = function(){
+        };
+        this.addItem = function(){
                         $scope.items.push({code:"", desc:"", qty:0,price:0,amount:0});
                         for (var i=0; i < $scope.items.length; i++) {
-                             $scope.total += $scope.items[i].amount;
+                             $scope.header.total += $scope.items[i].amount;
+                             console.log($scope.header.total);
                         };
-    };
-    this.emailInvoice = function(){
-        emailService.set($scope);
-        $location.path('/email');       
-    };
-    
-    });
+        };
+        this.getTotal = function(){
+            $scope.header.total = 0;
+            for (var i=0; i < $scope.items.length; i++) {
+                $scope.header.total += $scope.items[i].amount;
+            };
+            return $scope.header.total;
+        };
 
-    app.controller("itemList", function($scope, $document, emailService){
-        invoice       =  emailService.get();
-        $scope.header =  invoice.header;
-        $scope.items  =  invoice.items;
-
-        this.printDiv = function() {
+        this.emailInvoice = function(){
+             emailService.set($scope);
+        //$location.path('/email');
             var printContents = document.getElementById("invoice").innerHTML;
-            var popupWin = window.open('', '_blank', 'width=300mm,height=300');
+            var popupWin = window.open('', '_blank', 'width=500mm, height=600mm');
+            popupWin.var = 'khara'
             popupWin.document.open();
-            popupWin.document.write('<html id="a4"><head><link rel="stylesheet" href="css/bootstrap.min.css">' +
-                '<link rel="stylesheet" type="text/css" media="print" href="styles.css"></head>' +
-                '<body id="a4" onload="window.print()">' + printContents + '</body></html>');
+            popupWin.document.write('<html id="a4">' +
+                                    '<head><link rel="stylesheet" href="css/bootstrap.min.css">' +
+                                    '<link rel="stylesheet" type="text/css" media="print" href="styles.css"></head>' +
+                                    '<body id="a4" onload="window.print()">' +
+                                     printContents + popupWin.var + '<iframe src="test.html"></iframe>' +
+                                    '</body></html>');
             popupWin.document.close();
         };
     });
-    
+
+    app.controller("itemList", function($scope, $document, emailService){
+        var invoice   =  emailService.get();
+        $scope.header =  invoice.header;
+        $scope.items  =  invoice.items;
+    });
+
     app.factory('emailService', function(){
         var invoice = {};
         var get = function(){
             return invoice;
         };
         var set = function( $scope ){
-            invoice.header = $scope.header
+            invoice.header = $scope.header;
             invoice.items  = $scope.items;
         };
         return {get:get, set:set};
